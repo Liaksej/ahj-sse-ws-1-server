b3a0f55fb77bfce8d44775350b6225670d9e5559
Liaksej <tarusov.liaksej@gmail.com>
2023-08-17 14:56:57 +0200

    Add UserResponse class and refactor server code
    Implemented a new class "UserResponse" to standardize responses to user-related requests on the server. Refactored "app.js" to utilize UserResponse for more readable and maintainable code. The previous inline response object creation method was cluttered and not easily scalable.

--- server/app.js
+++ server/app.js
@@ -1,12 +1,8 @@
 const Koa = require("koa");
-const bodyParser = require("koa-bodyparser");
 const cors = require("@koa/cors");
-// const Ticket = require("./modules/Ticket");
+const UserResponse = require("./modules/UserResponse");
 const WS = require("ws");
 const http = require("http");
-
-// const tickets = [];
-// const ticketsFull = [];
 
 const app = new Koa();
 app.use(cors());
@@ -28,110 +24,6 @@ app.use(async (ctx, next) => {
   ctx.response.status = 204;
 });
 
-app.use(bodyParser());
-// app.use(async (ctx, next) => {
-//   const { method, id } = ctx.request.query;
-//   next();
-//   try {
-//     switch (method) {
-//       case "allTickets":
-//         ctx.body = tickets;
-//         break;
-//       case "ticketById":
-//         ctx.status = 200;
-//         ctx.body = ticketsFull.find((ticket) => ticket.id === id);
-//         break;
-//       case "editTicket":
-//         if (ctx.request.body) {
-//           let editedTicket = tickets.find((ticket) => {
-//             return ticket.id === id;
-//           });
-//           let editedTicketFull = ticketsFull.find((ticketFull) => {
-//             return ticketFull.id === id;
-//           });
-//           if (editedTicket) {
-//             if (editedTicket.name !== ctx.request.body.name) {
-//               editedTicket.name = ctx.request.body.name;
-//               editedTicketFull.name = ctx.request.body.name;
-//             }
-//             if (editedTicketFull.description !== ctx.request.body.description) {
-//               editedTicketFull.description = ctx.request.body.description;
-//             }
-//           }
-//           ctx.status = 200;
-//           ctx.body = tickets;
-//         } else {
-//           ctx.status = 400;
-//           ctx.body = "Отсутствует тело запроса";
-//         }
-//         break;
-//       case "changeStatus":
-//         if (ctx.request.body) {
-//           let editedTicket = tickets.find((ticket) => {
-//             return ticket.id === id;
-//           });
-//           let editedTicketFull = ticketsFull.find((ticketFull) => {
-//             return ticketFull.id === id;
-//           });
-//           if (editedTicket) {
-//             if (editedTicket.status !== ctx.request.body.status) {
-//               editedTicket.status = ctx.request.body.status;
-//               editedTicketFull.status = ctx.request.body.status;
-//             }
-//           }
-//           ctx.status = 200;
-//           ctx.body = editedTicket;
-//         } else {
-//           ctx.status = 400;
-//           ctx.body = "Отсутствует тело запроса";
-//         }
-//         break;
-//       case "createTicket":
-//         if (ctx.request.body) {
-//           let newTicket = new Ticket(
-//             ctx.request.body.name,
-//             ctx.request.body.status,
-//           );
-//           let newTicketFull = newTicket.createTicketFull(
-//             ctx.request.body.description,
-//           );
-//           tickets.push(newTicket);
-//           ticketsFull.push(newTicketFull);
-//           ctx.status = 201;
-//           ctx.body = tickets;
-//         } else {
-//           ctx.status = 400;
-//           ctx.body = "Отсутствует тело запроса";
-//         }
-//         break;
-//       case "deleteTicketById":
-//         if (ctx.request.body) {
-//           tickets.splice(
-//             tickets.findIndex((ticket) => ticket.id === id),
-//             1,
-//           );
-//           ticketsFull.splice(
-//             ticketsFull.findIndex((ticket) => ticket.id === id),
-//             1,
-//           );
-//           ctx.status = 200;
-//           ctx.body = tickets;
-//         } else {
-//           ctx.status = 400;
-//           ctx.body = "Отсутствует тело запроса";
-//         }
-//         break;
-//       default:
-//         ctx.status = 404;
-//         return;
-//     }
-//   } catch (err) {
-//     console.error(err);
-//     ctx.status = err.status || 500;
-//     ctx.body = err.message;
-//   }
-// });
-
 const server = http.createServer(app.callback());
 
 const wsServer = new WS.Server({
@@ -148,13 +40,14 @@ wsServer.on("connection", (ws) => {
       case "new-user":
         if (!users.includes(data.username)) {
           users.push(data.username);
-          const accessAllowed = {
-            type: "new-user-response",
-            success: "allowed",
-            username: data.username,
-          };
-          ws.send(JSON.stringify(accessAllowed));
+
+          ws.send(
+            JSON.stringify(new UserResponse("allowed", data.username, users)),
+          );
+
           ws.send(JSON.stringify({ type: "message", chat: chat }));
+        } else {
+          ws.send(JSON.stringify(new UserResponse("denied", data.username)));
         }
         break;
       case "message":
